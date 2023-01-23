e19f69a8e5b0c41a1d1b061dd32fc5fdb32129f3
My Computer <yokalen@gmail.com>
2023-01-22 19:52:03 -0800

    seperation of concerns

---
+++ main.js
@@ -0,0 +1,6 @@
+const rootElement = document.getElementById('root')
+const element = document.createElement('div')
+        
+element.textContent = 'Good morning, good afternoon, good evening - no matter where you\'re coming from: welcome.'
+element.className = 'container'
+rootElement.appendChild(element)
